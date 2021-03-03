c8ec40d260ebc4fcb3d4f8e9f7c66ca831ed33d6
Kai Hao <kevin830726@gmail.com>
2021-03-03 11:09:52 +0800

    Cleanup

--- packages/repro-issue/report/index.js
+++ packages/repro-issue/report/index.js
@@ -6,6 +6,27 @@ const path = require( 'path' );
 const core = require( '@actions/core' );
 const github = require( '@actions/github' );
 
+const LABELS = {
+	hasRepro: 'has-repro',
+	reproducible: 'reproducible',
+	notReproducible: 'not-reproducible',
+	brokenRepro: 'broken-repro',
+};
+
+function updateLabels( currentLabels, expectedLabels ) {
+	const labels = new Set( currentLabels );
+
+	Object.values( LABELS ).forEach( ( label ) => {
+		if ( expectedLabels.includes( label ) ) {
+			labels.add( label );
+		} else {
+			labels.delete( label );
+		}
+	} );
+
+	return Array.from( labels );
+}
+
 async function report() {
 	try {
 		const token = core.getInput( 'repo-token', { required: true } );
@@ -13,6 +34,10 @@ async function report() {
 		const issue = github.context.payload.issue;
 
 		let result;
+
+		const currentLabels = new Set(
+			issue.labels.map( ( label ) => label.name )
+		);
 
 		try {
 			const file = await fs.readFile(
@@ -22,25 +47,39 @@ async function report() {
 			result = JSON.parse( file );
 		} catch ( err ) {
 			core.info( 'No report file found, neutral exiting...' );
+
+			const labels = updateLabels( currentLabels, [] );
+
+			await client.issues.update( {
+				owner: github.context.repo.owner,
+				repo: github.context.repo.repo,
+				issue_number: issue.number,
+				labels,
+			} );
+
 			core.debug( err );
 			return;
 		}
 
 		let label;
-
 		if ( result.numRuntimeErrorTestSuites > 0 ) {
-			label = 'broken-repro';
+			label = LABELS.brokenRepro;
 		} else if ( result.success ) {
-			label = 'not-reproducible';
+			label = LABELS.notReproducible;
 		} else {
-			label = 'reproducible';
+			label = LABELS.reproducible;
 		}
 
-		await client.issues.addLabels( {
+		const labels = updateLabels( currentLabels, [
+			LABELS.hasRepro,
+			label,
+		] );
+
+		await client.issues.update( {
 			owner: github.context.repo.owner,
 			repo: github.context.repo.repo,
 			issue_number: issue.number,
-			labels: [ 'has-repro', label ],
+			labels,
 		} );
 	} catch ( error ) {
 		core.error( error );
